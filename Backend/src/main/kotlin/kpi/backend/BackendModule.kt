4de5a85b5672c6417dfde0ff66552f3d02021e37
Oleksandr <olieksandr.volovik@gmail.com>
2023-12-17 14:16:01 +0200

    refactor: Stop the application if the required environment variables are not set

--- Backend/src/main/kotlin/kpi/backend/BackendModule.kt
+++ Backend/src/main/kotlin/kpi/backend/BackendModule.kt
@@ -2,15 +2,31 @@ package kpi.backend
 
 import org.jetbrains.exposed.sql.Database
 import org.koin.dsl.module
+import kotlin.system.exitProcess
 
 val backendModule = module {
     single {
-        Database.connect(
-            url = "jdbc:postgresql://${System.getenv("POSTGRES_HOST")}:${System.getenv("POSTGRES_PORT")}/" +
-                    System.getenv("POSTGRES_DB_NAME"),
-            user = System.getenv("POSTGRES_USER"),
+        val host = System.getenv("POSTGRES_HOST")
+        val port = System.getenv("POSTGRES_PORT")
+        val dbName = System.getenv("POSTGRES_DB_NAME")
+        val username = System.getenv("POSTGRES_USER")
+        val password = System.getenv("POSTGRES_PASS")
+
+        if (host == null || port == null || dbName == null || username == null || password == null) {
+            println("Required environment variable(s) not found!")
+            println("POSTGRES_HOST = $host")
+            println("POSTGRES_PORT = $port")
+            println("POSTGRES_DB_NAME = $dbName")
+            println("POSTGRES_USER = $username")
+            println("POSTGRES_PASS = $password")
+            exitProcess(1)
+        }
+
+        return@single Database.connect(
+            url = "jdbc:postgresql://${host}:${port}/${dbName}",
+            user = username,
             driver = "org.postgresql.Driver",
-            password = System.getenv("POSTGRES_PASS")
+            password = password
         )
     }
     single { ViewerService(get()) }
--- EmployeesBackend/src/main/kotlin/kpi/employees_backend/EmployeesBackendModule.kt
+++ EmployeesBackend/src/main/kotlin/kpi/employees_backend/EmployeesBackendModule.kt
@@ -2,14 +2,30 @@ package kpi.employees_backend
 
 import com.mongodb.kotlin.client.coroutine.MongoClient
 import org.koin.dsl.module
+import kotlin.system.exitProcess
 
 val employeesBackendModule = module {
     single {
-        val connectionString = "mongodb://${System.getenv("MONGO_USER")}:${System.getenv("MONGO_PASS")}@${System.getenv("MONGO_HOST")}:${System.getenv("MONGO_PORT")}"
+        val host = System.getenv("MONGO_HOST")
+        val port = System.getenv("MONGO_PORT")
+        val dbName = System.getenv("MONGO_DB_NAME")
+        val username = System.getenv("MONGO_USER")
+        val password = System.getenv("MONGO_PASS")
+
+        if (host == null || port == null || dbName == null || username == null || password == null) {
+            println("Required environment variable(s) not found!")
+            println("MONGO_HOST = $host")
+            println("MONGO_PORT = $port")
+            println("MONGO_DB_NAME = $dbName")
+            println("MONGO_USER = $username")
+            println("MONGO_PASS = $password")
+            exitProcess(1)
+        }
+
+        val connectionString = "mongodb://$username:$password@$host:$port"
         val client = MongoClient.create(connectionString = connectionString)
 
-        val databaseName = System.getenv("MONGO_DB_NAME")
-        return@single client.getDatabase(databaseName = databaseName)
+        return@single client.getDatabase(databaseName = dbName)
     }
     single {
         EmployeeService(get())
